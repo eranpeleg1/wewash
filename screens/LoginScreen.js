ae1d88b26b4c9cb698626fe1b96cd7ee06ed21a3
Yoav Lahav <yoavl@wix.com>
2020-04-28 20:58:46 +0300

    redesigned logo svg

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -70,15 +70,10 @@ export default function LoginScreen({navigation}) {
     return (
         <ImageBackground source={background} style={styles.container}>
               <View style={styles.logoContainer}>
-              <Svg  width="100%" height="100%" viewBox="0 0 586.02 150.99">
-            <G>
-                <G>
-                <Path fill="white" d="M71.81 69.63h25.43L77.42 151h-25l-4.1-30.32L40.87 151H18.64L0 69.63h24.31l6.85 30 6.91-30h20l6.85 30zM159 69.63V92.2h-36.33V99H159v22.63h-36.33v6.74H159V151h-58.95V69.64h59zM254.17 69.63h25.43L259.78 151h-25l-4.1-30.32-7.45 30.32H201l-18.64-81.37h24.31l6.85 30 6.91-30h20l6.85 30zM341.92 69.63L373 151h-26l-3.09-8h-31.36l-3 8H282.4l32.23-81.35zM336 121.74l-7.69-20.32-7.69 20.32z" />
-                <Path fill="white" d="M402 92.2q.28.33 26.33 27.46a19.29 19.29 0 013.54 11 20.2 20.2 0 01-5.67 14.26 18.15 18.15 0 01-13.64 6.08h-41.77v-22.64h35.32L379.89 101a16.84 16.84 0 01-3.59-11 20.2 20.2 0 015.61-14.26 18 18 0 0113.64-6.06h39.69v22.53H402z" />
-                <Path fill="white" d="M481.5 69.63h22.57V151H481.5v-29.38h-24.2V151h-22.63V69.63h22.63V99h24.2zM561.45 88.85C561.45 62.28 586 62.6 586 62.6s-24.57.42-24.57-26.25c0 26.57-24.57 26.25-24.57 26.25s24.49-.32 24.59 26.25zM522.65 71.6c0-36.2 33.4-35.8 33.4-35.8s-33.4.4-33.4-35.8c0 36.2-33.4 35.8-33.4 35.8s33.4-.4 33.4 35.8zM531.45 98.93c0-16 14.63-15.73 14.63-15.73s-14.63.22-14.63-15.73c0 16-14.63 15.73-14.63 15.73s14.63-.11 14.63 15.73z" />
-                </G>
-            </G> 
-            </Svg>
+              <Svg width="100%" height="100%" viewBox="0 0 569 149">
+      <Path fill="white" d="M71.81 67.63h25.43L77.42 149h-25l-4.1-30.32L40.87 149H18.64L0 67.63h24.31l6.85 30 6.91-30h20l6.85 30zM159 67.63V90.2h-36.33V97H159v22.63h-36.33v6.74H159V149h-58.95V67.64h59zM254.17 67.63h25.43L259.78 149h-25l-4.1-30.32-7.45 30.32H201l-18.64-81.37h24.31l6.85 30 6.91-30h20l6.85 30zM321.92 67.63L353 149h-26l-3.09-8h-31.36l-3 8H262.4l32.23-81.35zM316 119.74l-7.69-20.32-7.69 20.32z" />
+      <Path fill="white" d="M383 90.2q.28.33 26.33 27.46a19.28 19.28 0 013.54 11 20.21 20.21 0 01-5.67 14.26 18.16 18.16 0 01-13.64 6.08h-41.77v-22.64h35.32L360.89 99a16.83 16.83 0 01-3.59-11 20.17 20.17 0 015.61-14.26 18 18 0 0113.64-6.06h39.69v22.53H383zM463.5 67.63h22.57V149H463.5v-29.38h-24.2V149h-22.63V67.63h22.63V97h24.2zM544.45 88.85C544.45 62.28 569 62.6 569 62.6s-24.57.42-24.57-26.25c0 26.57-24.57 26.25-24.57 26.25s24.49-.32 24.59 26.25zM505.65 71.6c0-36.2 33.4-35.8 33.4-35.8s-33.4.4-33.4-35.8c0 36.2-33.4 35.8-33.4 35.8s33.4-.4 33.4 35.8zM514.45 98.93c0-16 14.63-15.73 14.63-15.73s-14.63.22-14.63-15.73c0 16-14.63 15.73-14.63 15.73s14.63-.11 14.63 15.73z" />
+    </Svg>
             </View>
             <View style={styles.container}>
          
@@ -134,12 +129,6 @@ const styles = StyleSheet.create({
         alignItems:'center',
         justifyContent:'center'
     },
-    logoImage: {
-        top: Constants.statusBarHeight+100,
-        width:300,
-        height:200,
-        resizeMode:'contain',
-    },
     faceBookText: {
         color: '#ffffff',
         paddingLeft: paddingText,
@@ -150,27 +139,28 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
     buttons:{
-        top:'60%',
+        width:'100%',
+        top:'7%',
         height:'100%'
     },
     fbButton:{
         borderWidth:0,
         borderColor:'transparent',
-        width:240,
+        width:screenWidth*0.60,
         height:50,
         backgroundColor:"#3b5998"
     },
     buttonGoogle:{
         borderWidth:0,
         borderColor:'transparent',
-        width:240,
+        width: screenWidth*0.60,
         height:50,
         backgroundColor:"#4c8bf5"
     },
     logoContainer:{
         flex:1,
-        width:'66%',
-        marginRight: Platform.OS !== 'ios' ? `${(66*0.1919)/2}%` : 0,
-        marginLeft: Platform.OS === 'ios' ? `${(66*0.1919)/2}%` : 0
+        width:screenWidth*0.60,
+        marginRight: Platform.OS !== 'ios' ? `${(66*0.1919)}%` : 0,
+        marginLeft: Platform.OS === 'ios' ? `${(66*0.1919)/1.45}%` : 0
     }
 });
